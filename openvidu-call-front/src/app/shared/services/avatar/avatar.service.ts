93939af49cd26fd33b9cf676a6c9a5609f50de64
Raul Mendoza <raul_fernando_mendoza@hotmail.com>
2021-05-27 17:09:30 -0500

    removing images of openvidu

--- openvidu-call-front/src/app/shared/services/avatar/avatar.service.ts
+++ openvidu-call-front/src/app/shared/services/avatar/avatar.service.ts
@@ -8,7 +8,7 @@ import { ILogger } from '../../types/logger-type';
 	providedIn: 'root'
 })
 export class AvatarService {
-	private openviduAvatar = 'assets/images/openvidu_globe.png';
+	private openviduAvatar = 'assets/images/rax_logo_no_text.png';
 	private capturedAvatar = '';
 	private log: ILogger;
 
